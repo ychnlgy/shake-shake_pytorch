432815aba08c59508d475b9c8f595aed040aef4e
Yuchen Li <ychnlgy@gmail.com>
2019-05-19 13:21:41 -0400

    Using shake shake when not MNIST

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main(repeat):
         for mnist in [0, 1]:
             for act_type in ["relu", "regact", "linkact"]:
                 os.system(
-                    "python3 train.py --mnist %d --act_type %s --use_shakeshake %d" % (
+                    "python3 train.py --mnist %d --act_type %s --use_shakeshake %d --epochs 1" % (
                         mnist, act_type, 1-mnist
                     )
                 )
