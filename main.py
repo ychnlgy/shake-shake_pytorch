7d6b135b23bf9f69d3be163d8b3c440ca6236cb2
Yuchen Li <ychnlgy@gmail.com>
2019-05-20 20:25:53 -0400

    Doing link act only

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os, sys
 def main(repeat):
     for repeat in range(repeat):
         for mnist in [1, 0]:
-            for act_type in ["linkact", "relu", "regact"]:
+            for act_type in ["linkact"]:#, "relu", "regact"]:
                 os.system(
                     "python3 train.py --mnist %d --act_type %s --use_shakeshake %d --epochs 100" % (
                         mnist, act_type, 1-mnist
