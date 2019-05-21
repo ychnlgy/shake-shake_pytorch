ffc53ef49eb30f1a95550a6b6133a015fcb9f73b
Yuchen Li <ychnlgy@gmail.com>
2019-05-21 16:56:29 -0400

    Restored original code

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os, sys
 
 def main(repeat):
     for repeat in range(repeat):
-        for mnist in [0]:
-            for act_type in ["linkact", "regact"]:#, "relu", "regact"]:
+        for mnist in [0, 1]:
+            for act_type in ["linkact", "regact", "relu"]:
                 os.system(
                     "python3 train.py --mnist %d --act_type %s --use_shakeshake %d --epochs 100" % (
                         mnist, act_type, 1-mnist
